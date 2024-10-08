b835eb3a6c70f1f9d0fa5b0e94e35c8a07a88ee8
Swarnika Sellathurai <swarnika.s@Swarnikas-MacBook-Air.local>
2024-10-08 20:32:14 +0200

    tok bort apostrof

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
 export default function HomePage(){
-  return <h1 className="text-center p-10"> Swarnika's nettside </h1>;
+  return <h1 className="text-center p-10"> Swarnikas nettside </h1>;
   
 }
